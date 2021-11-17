dda82cf42149a88e30c7b14af1d6fdc7fd990db3
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2021-11-17 08:37:59 +0100

    Add script to create decay 3 pars folders from Jonathon's ones

---
+++ scripts/create_SMEFTsim_folder.sh
@@ -0,0 +1,8 @@
+cd cards
+for i in H_*_SMEFT;
+do
+    cp -r ${i} ${i}sim-3pars
+    rm ${i}sim-3pars/reweight_card.dat ${i}sim-3pars/param_card.dat
+    sed -i 's/SMEFTsim_A_U35_MwScheme_UFO_v2_1/SMEFTsim_U35_MwScheme_UFO/g' ${i}sim-3pars/proc_card.dat
+    sed -i "s/$i/${i}sim-3pars/g" ${i}sim-3pars/proc_card.dat
+done
