97ad09d32f52014e54cf7ab86edbf3e51cdf5407
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2021-11-18 09:26:46 +0100

    Change jet merging in the script and mu mu

--- scripts/create_SMEFTsim_folder.sh
+++ scripts/create_SMEFTsim_folder.sh
@@ -5,4 +5,5 @@ do
     rm ${i}sim-3pars/reweight_card.dat ${i}sim-3pars/param_card.dat
     sed -i 's/SMEFTsim_A_U35_MwScheme_UFO_v2_1/SMEFTsim_U35_MwScheme_UFO/g' ${i}sim-3pars/proc_card.dat
     sed -i "s/$i/${i}sim-3pars/g" ${i}sim-3pars/proc_card.dat
+    sed -i "s/JetMatching:merge = on/JetMatching:merge = off/g" ${i}sim-3pars/pythia8_card.dat
 done
